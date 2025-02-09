ac0f2f58b1f4b300ae5383813ddc9ffc1cff41ff
Seobkim-Desktop-Win <wnstjqaodls@gmail.com>
2025-02-09 22:38:52 +0900

    :new: first Page

--- src/components/vMypage.vue
+++ src/components/vMypage.vue
@@ -1,7 +1,25 @@
 <template>
     <h1>THIS IS PAGE</h1>
+    <div class="text-h1 primary">ABOUT ME</div>
+    <hr />
+  <div class="row bg-primary">
+    <div class="col-xs-12 col-sm-6">게시글 제목</div>
+
+
+    <div class="col-2">작성자</div>
+    <div class="col-auto">작성일</div>
+  </div>
+  <div class="row">
+    <div class="col-6">게시글 내용
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste provident exercitationem, illum ad nam quo. Obcaecati, optio dignissimos quos culpa, praesentium adipisci quasi qui amet aperiam quod iste quas natus?</p>
+    </div>
+    <input type="text" class="col-6" />
+    <button class="col-6 btn btn-danger">Save</button>
+  </div>
 </template>
 <script setup>
+
+
 
 </script>
 <style scoped>
